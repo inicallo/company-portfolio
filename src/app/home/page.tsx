82f9e00a4d3b2dee010d19c87783c6b3158aaefb
Calvin Angelo <calvinangelo@Calvins-MacBook-Pro.local>
2024-08-09 02:46:59 +0800

    update home screen

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -17,8 +17,8 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="relative mt-5 flex items-center justify-center min-h-screen pt-16">
-      <div className="relative h-screen w-full overflow-hidden">
+    <section className="relative flex items-center justify-center min-h-screen">
+      <div className=" h-screen w-full overflow-hidden">
         {images.map((image, index) => (
           <div
             key={index}
@@ -33,16 +33,19 @@ const Hero = () => {
       </div>
 
       {/* Content */}
-      <div className="absolute lg:mt-60 mt-16 text-center inset-0 flex flex-col justify-center items-center p-8 md:p-12 lg:p-20 lg:items-start lg:text-left lg:justify-center z-20">
+      <div className="absolute lg:mt-72 mt-20 text-center inset-0 flex flex-col justify-center items-center p-8 md:p-12 lg:p-20 lg:items-start lg:text-left lg:justify-center z-20">
         <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-gray-800 mb-2 lg:mt-80">
-          Capture Your Brand&apos;s <br />Story Globally
+          Capture Your Brand&apos;s <br />
+          Story Globally
         </h1>
         <p className="text-[12px] md:text-lg lg:text-xl italic text-gray-950 mb-5 text-center lg:text-left">
-          Get tailored photography and video services<br />
-          anywhere in the world with our network of local experts,<br />
+          Get tailored photography and video services
+          <br />
+          anywhere in the world with our network of local experts,
+          <br />
           ensuring top-quality visuals every time
         </p>
-        <div className="flex flex-col items-center justify-end h-full w-full mb-32 lg:flex-row-reverse lg:mb-80">
+        <div className="flex flex-col items-center justify-end h-full w-full mb-14 lg:flex-row-reverse lg:mb-80">
           <Link href={"https://wa.me/+628111628881"} target="_blank">
             <Button type="button" title="Get In Touch" variant="btn_dark_red" />
           </Link>
